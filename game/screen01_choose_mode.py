79e8ff0b769d797d497c4e7880aff51f23bee218
sambano <sambano@debian>
2023-10-10 16:54:00 +0200

    upload test

--- game/screen01_choose_mode.py
+++ game/screen01_choose_mode.py
@@ -12,6 +12,7 @@ from classes.utilitaires.utilitaires_01 import Utilitaires01
 
 from logger_config import configure_logger
 # méthodes du logger (debug(), info(), warning(), error(), critical())
+# TEST
 logger = configure_logger(__name__, 'logs/screen01_choose_mode.log')
 
 
